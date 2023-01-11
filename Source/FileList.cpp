9eeb1c3fe9ebe27d42572c83047b18ebbca3b126
unknown <lucienharryholloway@hotmail.co.uk>
2023-01-11 13:21:30 +0000

    Don't include `<sys/io.h>` on ARM architecture

--- Source/FileList.cpp
+++ Source/FileList.cpp
@@ -20,7 +20,7 @@
 #elif defined(_WIN32) || defined(__CYGWIN__)
 #include <io.h>
 
-#elif !defined(__APPLE__) && !defined(__APPLE_CC__) && !defined(__PPC__) && !defined(__FreeBSD__) && !defined(__S3E__)
+#elif !defined(__APPLE__) && !defined(__APPLE_CC__) && !defined(__PPC__) && !defined(__FreeBSD__) && !defined(__S3E__) && !defined(__arm__) && !defined(__aarch64__)
 #include <sys/io.h>
 #endif
 
